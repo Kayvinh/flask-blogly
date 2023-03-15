7f9648be0705a3c0b7eafa7a4d47921473ac8253
Kevin Nguyen <kayvinhnguyen@gmail.com>
2023-03-15 12:30:29 -0700

    finishing creating base template

--- models.py
+++ models.py
@@ -1,1 +1,36 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """ Connect to database. """
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
 """Models for Blogly."""
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    first_name = db.Column(
+        db.String(30),
+        nullable=False
+    )
+
+    last_name = db.Column(
+        db.String(30),
+        nullable=False
+    )
+
+    image_url = db.Column(
+        db.String(200),
+        nullable=True,
+        default="http://cdn.shopify.com/s/files/1/1061/1924/products/Emoji_Icon_-_Clown_emoji_grande.png?v=1571606089"
+    )
+

21b2b64b8175a3bcf2750fc177d49c88ee7f63fa
Vaughn Seekamp <vseekamp@gmail.com>
2023-03-16 16:14:34 -0700

    now with seed

--- seed.py
+++ seed.py
@@ -20,10 +20,12 @@ db.session.commit()
 # Add new posts
 post1 = Post(title='A cool title', content= 'A cool article', user_id=user1.id)
 post2 = Post(title='A lame title', content= 'A lame article', user_id=user1.id)
+post3 = Post(title='A very lame title', content= 'A lame article', user_id=user2.id)
 
 # Add posts to session
 db.session.add(post1)
 db.session.add(post2)
+db.session.add(post3)
 
 # commit
 db.session.commit()
